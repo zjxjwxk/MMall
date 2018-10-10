60d65547a405bcc6dafba9980e1328b4af6827c7
zjxjwxk <zjxjwxk@gmail.com>
2018-10-10 23:08:20 +0800

    Controller

---
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -0,0 +1,41 @@
+package com.mmall.controller.portal;
+
+import com.mmall.common.Const;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author zjxjwxk
+ */
+@Controller
+@RequestMapping("/user/")
+public class UserController {
+
+    private IUserService iUserService;
+
+    /**
+     * 用户登陆
+     * @param username 用户名
+     * @param password 密码
+     * @param session 会话
+     * @return 响应
+     */
+    @RequestMapping(value = "login.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> login(String username, String password, HttpSession session) {
+        //service->mybatis->dao
+        ServerResponse<User> response = iUserService.login(username, password);
+        if (response.isSuccess()) {
+            session.setAttribute(Const.CURRENT_USER, response.getData());
+        }
+
+        return response;
+    }
+}
